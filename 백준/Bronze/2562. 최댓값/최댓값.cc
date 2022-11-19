964c33ad16b18458eb66256f5a48f54776b81ef4
batsalee <109213754+batsalee@users.noreply.github.com>
2022-11-20 06:26:19 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{	
+	vector<int> inputs(9);
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> inputs[i];
+	}	
+
+	cout << *max_element(inputs.begin(), inputs.end());
+	cout << endl;
+	cout << max_element(inputs.begin(), inputs.end()) - inputs.begin() + 1;
+
+	return 0;
+}
