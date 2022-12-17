c62bbb2e6fc2c5c618d24a6f990a457710f6dbdc
batsalee <109213754+batsalee@users.noreply.github.com>
2022-12-17 15:56:41 +0900

    Update 신나는 함수 실행.cc

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -5,6 +5,52 @@ int result[21][21][21];
 int w(int a, int b, int c)
 {
 	if (a <= 0 || b <= 0 || c <= 0) return 1;
+	if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+
+	int temp1, temp2, temp3, temp4;
+	if (a < b && b < c) {
+		if (result[a][b][c - 1]) temp1 = result[a][b][c - 1];
+		else temp1 = w(a, b, c - 1);
+		if (result[a][b - 1][c - 1]) temp2 = result[a][b - 1][c - 1];
+		else temp2 = w(a, b - 1, c - 1);
+		if (result[a][b - 1][c]) temp3 = result[a][b - 1][c];
+		else temp3 = w(a, b - 1, c);
+		result[a][b][c] = temp1 + temp2 - temp3;
+		return result[a][b][c];
+	}
+
+	if (result[a - 1][b][c]) temp1 = result[a - 1][b][c];
+	else temp1 = w(a - 1, b, c);
+	if (result[a - 1][b - 1][c]) temp2 = result[a - 1][b - 1][c];
+	else temp2 = w(a - 1, b - 1, c);
+	if (result[a - 1][b][c - 1]) temp3 = result[a - 1][b][c - 1];
+	else temp3 = w(a - 1, b, c - 1);
+	if (result[a - 1][b - 1][c - 1]) temp4 = result[a - 1][b - 1][c - 1];
+	else temp4 = w(a - 1, b - 1, c - 1);
+	result[a][b][c] = temp1 + temp2 + temp3 - temp4;
+	return result[a][b][c];
+}
+
+int main()
+{
+	int a, b, c;
+	while (true) {
+		std::cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1) break;
+		std::cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << '\n';
+	}
+
+	return 0;
+}
+
+/*
+#include <iostream>
+
+int result[21][21][21];
+
+int w(int a, int b, int c)
+{
+	if (a <= 0 || b <= 0 || c <= 0) return 1;
 	if (a > 20 || b > 20 || c > 20) return result[20][20][20];
 	return result[a][b][c];
 }
@@ -30,3 +76,4 @@ int main()
 
 	return 0;
 }
+*/
