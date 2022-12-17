19cdc7bd20a038fc844f4ccb539b09a6542bd204
batsalee <109213754+batsalee@users.noreply.github.com>
2022-12-17 14:00:56 +0900

    [Silver II] Title: 좌표 압축, Time: 984 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <set>
+#include <algorithm>
 
 int main()
 {
@@ -8,12 +8,16 @@ int main()
 	std::cin >> number_of_coordinate;
 
 	std::vector<int> coordinates(number_of_coordinate);
+	std::vector<int> sorted(number_of_coordinate);
+	int input;
 	for (int i = 0; i < number_of_coordinate; i++) {
-		std::cin >> coordinates[i];
+		std::cin >> input;
+		coordinates[i] = input;
+		sorted[i] = input;		
 	}
+	sort(sorted.begin(), sorted.end());
+	sorted.erase(std::unique(sorted.begin(), sorted.end()), sorted.end());
 
-	std::set<int> for_sorting(coordinates.begin(), coordinates.end());
-	std::vector<int> sorted(for_sorting.begin(), for_sorting.end());
 	for (int& i : coordinates) {
 		std::cout << std::lower_bound(sorted.begin(), sorted.end(), i) - sorted.begin() << ' '; 
 	}
