c0d932c35702a19b07d6c1059c94f3cb5b84bb89
batsalee <109213754+batsalee@users.noreply.github.com>
2022-12-27 16:53:56 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;    
+    for(const auto& a : array)
+        answer += (a > height);
+        
+    return answer;
+}
