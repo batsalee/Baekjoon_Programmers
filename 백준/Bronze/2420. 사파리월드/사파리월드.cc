b1c15aa0652d995b80226273fe9a954071231c93
batsalee <109213754+batsalee@users.noreply.github.com>
2022-12-28 20:08:20 +0900

    Update 사파리월드.cc

--- 백준/Bronze/2420. 사파리월드/사파리월드.cc
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -5,7 +5,7 @@ int main()
     long long DoF1, DoF2; // Degree of Famous
     std::cin >> DoF1 >> DoF2;
 
-    std::cout << llabs(DoF1 - DoF2);
+    std::cout << std::abs(DoF1 - DoF2);
 
     return 0;
 }
