51430458b85c04f2514265db6be823f5ef425833
batsalee <109213754+batsalee@users.noreply.github.com>
2023-02-07 13:02:15 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	std::string input;
+	std::cin >> input;
+
+	std::cout << ":fan::fan::fan:\n";
+	std::cout << ":fan::" << input << "::fan:\n";
+	std::cout << ":fan::fan::fan:\n";
+
+	return 0;
+}
