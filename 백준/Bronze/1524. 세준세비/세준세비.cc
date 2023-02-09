66f27bd1af8c6701cb0900724f632404051795a4
batsalee <109213754+batsalee@users.noreply.github.com>
2023-02-09 13:57:22 +0900

    [Bronze I] Title: 세준세비, Time: 72 ms, Memory: 2708 KB -BaekjoonHub

---
+++ 백준/Bronze/1524. 세준세비/세준세비.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int T;
+	std::cin >> T;
+
+	int N, M;
+	while (T--) {
+		//std::cout << '\n';
+		std::cin >> N >> M;
+		
+		std::vector<int> S(N), B(M);
+		for (int i = 0; i < N; i++) std::cin >> S[i];
+		for (int i = 0; i < M; i++) std::cin >> B[i];
+
+		sort(S.begin(), S.end());
+		sort(B.begin(), B.end());
+
+		int Si = 0, Bi = 0;
+		for (int i = 0; i < N + M; i++) {
+			if (S[Si] >= B[Bi]) Bi++;
+			else Si++;
+
+			if (Si == N) {
+				std::cout << "B\n";
+				break;
+			}
+
+			if (Bi == M) {
+				std::cout << "S\n";
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
