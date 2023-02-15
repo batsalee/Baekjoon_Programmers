6ae693f72caefce7299d53e0241fe678999c2fba
batsalee <109213754+batsalee@users.noreply.github.com>
2023-02-15 16:36:15 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 100 ms, Memory: 9552 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+std::vector<int> visited;
+std::vector<std::vector<int>> adj;
+
+void bfs(int u)
+{
+	static int order = 1;
+	visited[u] = order++;
+
+	std::queue<int> q;
+	q.push(u);
+
+	while (q.size()) {
+		int here = q.front();
+		q.pop();
+		for (int there : adj[here]) {
+			if (visited[there]) continue;
+
+			visited[there] = order++;
+			q.push(there);
+		}
+	}
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	
+	int number_of_vertex, number_of_edge, start_vertex;
+	std::cin >> number_of_vertex >> number_of_edge >> start_vertex;
+
+	visited.resize(number_of_vertex + 1);
+	adj.resize(number_of_vertex + 1);
+
+	for (int i = 0; i < number_of_edge; i++) {
+		int v, u;
+		std::cin >> v >> u;
+		adj[v].push_back(u);
+		adj[u].push_back(v);
+	}
+
+	for (auto& a : adj) {
+		sort(a.begin(), a.end());
+	}
+
+	bfs(start_vertex);
+	for (int i = 1; i <= number_of_vertex; i++) {
+		std::cout << visited[i] << '\n';
+	}
+	
+	return 0;
+}
