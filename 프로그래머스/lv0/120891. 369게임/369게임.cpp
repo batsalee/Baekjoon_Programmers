d995f5d46f58977c17306fbad7d502bea39e71ce
batsalee <109213754+batsalee@users.noreply.github.com>
2023-03-29 02:22:01 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.cpp
@@ -0,0 +1,11 @@
+int solution(int order) {
+    int answer = 0;
+    
+    while(order) {
+        int num = order % 10;
+        if(num == 3 || num == 6 || num == 9) answer++;
+        order /= 10;
+    }
+    
+    return answer;
+}
