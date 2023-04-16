8d710946de64d7cbc7473415bd0b0b2143466f0b
batsalee <109213754+batsalee@users.noreply.github.com>
2023-04-17 00:30:28 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -7,7 +7,7 @@ int main()
 	std::cin >> N;
 
 	for (int i = 1; i <= N; i++) {
-		for (int j = N - i; j > 0; j--) { std::cout << ' '; }
+		std::cout << std::string(N - i, ' ');
 		std::cout << std::string(2 * i - 1, '*') << '\n';
 	}
 
