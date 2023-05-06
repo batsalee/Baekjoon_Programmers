f5d8dcea630d832b2341bdb9cbd8bd2c3861ef06
batsalee <109213754+batsalee@users.noreply.github.com>
2023-05-06 10:40:32 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	int N;
+	std::cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		std::cout << std::string(N - i, ' ');
+		std::cout << std::string(i, '*');
+		std::cout << '\n';
+	}
+	for (int i = N - 1; i > 0; i--) {
+		std::cout << std::string(N - i, ' ');
+		std::cout << std::string(i, '*');
+		std::cout << '\n';
+	}
+
+	return 0;
+}
