99921e2d5aa6ff65a292306a8da7070929ce505f
batsalee <109213754+batsalee@users.noreply.github.com>
2023-06-12 21:34:05 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -1,27 +1,26 @@
 #include <iostream>
-#include <vector>
 
-std::vector<long long> fibonacci;
+#define MAX 90
 
-long long fibo_cal(int n)
+long long fibonacci[MAX + 1];
+
+void fibo_cal()
 {
 	fibonacci[0] = 0;
 	fibonacci[1] = 1;
 
-	for (int i = 2; i <= n; i++) {
+	for (int i = 2; i <= MAX; i++) {
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 	}
-
-	return fibonacci[n];
 }
 
 int main()
 {
+	fibo_cal();
+
 	int n;
 	std::cin >> n;
-	fibonacci.resize(n + 2);
-
-	std::cout << fibo_cal(n);
+	std::cout << fibonacci[n];
 
 	return 0;
 }
