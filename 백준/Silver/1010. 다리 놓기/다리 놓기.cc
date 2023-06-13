5b3f562600b5939180ba77bdd268398d3fa833d1
batsalee <109213754+batsalee@users.noreply.github.com>
2023-06-14 05:51:13 +0900

    Update 다리 놓기.cc

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -8,7 +8,7 @@ int main()
 
 	while (T--) {
 		int N, M;
-		std::cin >> N >> M;			
+		std::cin >> N >> M;
 
 		long long f[30] = { 0 };
 		f[0] = 1;
@@ -21,3 +21,29 @@ int main()
 
 	return 0;
 }
+
+/* 생각 공간
+* 
+f(0)
+N C N
+= N! / N! / 0!
+
+f(1)
+N C N-1
+ = N! / N-1! / 1!
+
+f(2)
+N+1 C N-1
+= (N+1)! / N-1 ! /2!
+
+f(3)
+N+2 C N-1
+= N+2 ! / N-1! / 3!
+
+f(1) = f(0) * N / 1 = N
+f(2) = N * (N+1) / 2
+f(3) = f(2) * (n+2) / 3
+
+정리하면 f(i) = f(i-1) * (N+i-1) / i
+
+*/
