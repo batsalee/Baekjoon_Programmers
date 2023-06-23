7c67746774dd2a6f11589c4bd1fd6e6e5d606ccc
batsalee <109213754+batsalee@users.noreply.github.com>
2023-06-23 19:44:17 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	cout << (2 << (N - 1));
+
+	return 0;
+}
