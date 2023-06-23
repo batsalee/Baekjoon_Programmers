210c1f9684b5c8c85416334f0863586ea055783b
batsalee <109213754+batsalee@users.noreply.github.com>
2023-06-23 23:14:03 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int N;
+	cin >> N;
+	cout << floor(sqrt(N));
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
