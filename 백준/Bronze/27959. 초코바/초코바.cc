1efc5aa300f3e2f3f4757f701aa54d682a6714c0
batsalee <109213754+batsalee@users.noreply.github.com>
2023-07-01 16:43:42 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int N, M;
+	cin >> N >> M;
+	cout << ((100 * N >= M) ? "Yes" : "No");
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
