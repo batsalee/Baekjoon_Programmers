247905b7093649cd9311d5027fb0af69903e8aa3
batsalee <109213754+batsalee@users.noreply.github.com>
2023-07-16 16:13:03 +0900

    [Bronze II] Title: 16진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int num;
+	scanf("%x", &num);
+	printf("%d", num);
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
