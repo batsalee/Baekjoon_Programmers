17259a39f4e0d2f8f8a79481694c438861d33a98
batsalee <109213754+batsalee@users.noreply.github.com>
2023-07-23 10:52:23 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int N, M;
+	cin >> N >> M;
+
+	cout << min(N, M) / 2;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
