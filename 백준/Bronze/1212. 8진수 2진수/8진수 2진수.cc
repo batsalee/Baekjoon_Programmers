6c07094c90c12f5b8daea8fe6488bd146c3ba6f5
batsalee <109213754+batsalee@users.noreply.github.com>
2023-08-01 13:42:31 +0900

    [Bronze II] Title: 8진수 2진수, Time: 16 ms, Memory: 2912 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.cc
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.cc
@@ -6,24 +6,8 @@ void solve()
 	string input;
 	cin >> input;
 
-	string oct[8] = { "000", "001", "010", "011", "100", "101", "110", "111" };
-	switch (input[0] - '0') {
-	case 0:
-		cout << 0;
-		break;
-	case 1:
-		cout << 1;
-		break;
-	case 2:
-		cout << 10;
-		break;
-	case 3:
-		cout << 11;
-		break;
-	default:
-		cout << oct[input[0] - '0'];
-	}
-
+	string oct[8] = { "000", "001", "010", "011", "100", "101", "110", "111" };	
+	cout << stoi(oct[input[0] - '0']);
 	for (int i = 1; i < input.length(); i++) {
 		cout << oct[input[i] - '0'];
 	}
