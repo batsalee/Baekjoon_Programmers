34a5de93fc55ce97445a1f7f1057ecdd7887b79a
batsalee <109213754+batsalee@users.noreply.github.com>
2023-08-21 02:59:53 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int R1, S;
+	cin >> R1 >> S;
+	cout << S * 2 - R1;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
