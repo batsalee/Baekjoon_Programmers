8c64b78fccd65b9dcc06872854819169f93570de
batsalee <109213754+batsalee@users.noreply.github.com>
2023-09-29 20:50:02 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int W, H;
+	cin >> W >> H;
+
+	cout.precision(1);
+	cout << fixed;
+	cout << W * H / 2.0;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
