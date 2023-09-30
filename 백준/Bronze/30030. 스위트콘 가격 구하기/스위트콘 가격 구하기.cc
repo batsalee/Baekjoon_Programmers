219ffb8b8be023ca334890d1bf866c6da59821a4
batsalee <109213754+batsalee@users.noreply.github.com>
2023-09-30 10:36:23 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int B;
+	cin >> B;
+	cout << B / 11 * 10;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
