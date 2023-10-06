5a8f2abf7b28773b6d9a01f10c42d4c54c2fd4e3
batsalee <109213754+batsalee@users.noreply.github.com>
2023-10-06 15:33:21 +0900

    Update DKSH 찾기.cc

--- 백준/Bronze/29766. DKSH 찾기/DKSH 찾기.cc
+++ 백준/Bronze/29766. DKSH 찾기/DKSH 찾기.cc
@@ -3,20 +3,16 @@ using namespace std;
 
 void solve()
 {
-	const char* DKSH = "DKSH";
-	
 	string input;
 	cin >> input;
-	if (input.length() < 4) {
-		cout << 0;
-		return;
-	}
-
+	
 	int count = 0;
-	for (int i = 0; i <= input.length() - 4; i++) {
-		if (input[i] == 'D' && input[i + 1] == 'K' && input[i + 2] == 'S' && input[i + 3] == 'H') count++;
-	}
-
+	if (input.size() >= 4) {
+		for (int i = 0; i <= input.size() - 4; i++) {
+			if (input.substr(i, 4) == "DKSH") count++;
+		}
+	}	
+		
 	cout << count;
 }
 
