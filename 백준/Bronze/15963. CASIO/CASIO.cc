e70f6539d9a48cc530bbf6179cb543f1d3033b47
batsalee <109213754+batsalee@users.noreply.github.com>
2023-10-12 02:36:53 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	long long N, M;
+	cin >> N >> M;
+	cout << (N == M);
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
