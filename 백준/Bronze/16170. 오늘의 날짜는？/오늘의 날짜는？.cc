c27dd8764cde64e1581c5bc044d1b23522f10545
batsalee <109213754+batsalee@users.noreply.github.com>
2023-12-13 09:25:52 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{	
+	cout << 2023 << '\n' << 12 << '\n' << 13;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
