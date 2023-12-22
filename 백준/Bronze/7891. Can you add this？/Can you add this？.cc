3e618d8921c0e2fc7a43317d2d930bc01804cb67
batsalee <109213754+batsalee@users.noreply.github.com>
2023-12-22 18:00:13 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int N;
+	cin >> N;
+
+	int a, b;
+	while (N--) {
+		cin >> a >> b;
+		cout << a + b << '\n';
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
