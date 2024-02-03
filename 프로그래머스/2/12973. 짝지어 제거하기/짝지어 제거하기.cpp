1ee329cff87bd70297af79deee0ec3fbdc59fe31
batsalee <109213754+batsalee@users.noreply.github.com>
2024-02-03 18:23:29 +0900

    [level 2] Title: 짝지어 제거하기, Time: 4.67 ms, Memory: 6.36 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -6,11 +6,8 @@ int solution(string s)
 {
     stack<char> st;
     for(char& c : s) {
-        if(st.empty()) st.push(c);
-        else {
-            if(st.top() == c) st.pop();
-            else st.push(c);
-        }
+        if(!st.empty() && st.top() == c) st.pop();
+        else st.push(c);
     }
     
     return st.empty();
