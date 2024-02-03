57864979473472d59625f9ac73fe7056606a166d
batsalee <109213754+batsalee@users.noreply.github.com>
2024-02-04 06:08:44 +0900

    [level 2] Title: 구명보트, Time: 1.35 ms, Memory: 4.54 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+	int answer = 0;
+	sort(people.begin(), people.end());
+	
+	int l = 0, r = people.size() - 1;
+	while (l <= r) {
+		if (people[l] + people[r] <= limit) {
+			l++; r--; answer++;
+		}
+		else {
+			r--; answer++;
+		}
+	}
+
+    return answer;
+}
