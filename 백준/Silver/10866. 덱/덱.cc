2743906b6744d296e4e71a282a7f5c78b530ba92
batsalee <109213754+batsalee@users.noreply.github.com>
2024-02-04 11:55:12 +0900

    [Silver IV] Title: 덱, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	deque<int> dq;
+	string input;
+	int num;
+	while (N--) {
+		cin >> input;
+		if (input == "push_front") {
+			cin >> num;
+			dq.push_front(num);
+		}
+		else if (input == "push_back") {
+			cin >> num;
+			dq.push_back(num);
+		}
+		else if (input == "pop_front") {
+			if (dq.empty()) cout << -1 << '\n';
+			else {
+				cout << dq.front() << '\n';
+				dq.pop_front();
+			}
+		}
+		else if (input == "pop_back") {
+			if (dq.empty()) cout << -1 << '\n';
+			else {
+				cout << dq.back() << '\n';
+				dq.pop_back();
+			}
+		}
+		else if (input == "size") {
+			cout << dq.size() << '\n';
+		}
+		else if (input == "empty") {
+			cout << dq.empty() << '\n';
+		}
+		else if (input == "front") {
+			if (dq.empty()) cout << -1 << '\n';
+			else cout << dq.front() << '\n';
+		}
+		else if (input == "back") {
+			if (dq.empty()) cout << -1 << '\n';
+			else cout << dq.back() << '\n';
+		}
+	}
+
+	return 0;
+}
