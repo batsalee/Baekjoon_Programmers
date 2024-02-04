4ad4c0828fda7776794ae164e5c911af87159e01
batsalee <109213754+batsalee@users.noreply.github.com>
2024-02-04 14:20:28 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	priority_queue<int, vector<int>, greater<> > pq;
+	int x;
+	while (N--) {
+		cin >> x;
+		if (x == 0) {
+			if (pq.empty()) cout << 0 << '\n';
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else pq.push(x);
+	}
+
+	return 0;
+}
