c48cbe54a94b6780af651b363315e182b1f3a2a5
batsalee <109213754+batsalee@users.noreply.github.com>
2024-02-04 11:51:55 +0900

    [Silver IV] Title: 큐, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	queue<int> q;
+	string input;
+	int num;
+	while (N--) {
+		cin >> input;
+		if (input == "push") {
+			cin >> num;
+			q.push(num);
+		}
+		else if (input == "pop") {
+			if (q.empty()) cout << -1 << '\n';
+			else {
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (input == "size") {
+			cout << q.size() << '\n';
+		}
+		else if (input == "empty") {
+			cout << q.empty() << '\n';
+		}
+		else if (input == "front") {
+			if (q.empty()) cout << -1 << '\n';
+			else cout << q.front() << '\n';
+		}
+		else if (input == "back") {
+			if (q.empty()) cout << -1 << '\n';
+			else cout << q.back() << '\n';
+		}
+	}
+
+	return 0;
+}
