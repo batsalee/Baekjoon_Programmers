46caa774677687617218367c8b51507241e571f1
batsalee <109213754+batsalee@users.noreply.github.com>
2024-02-18 21:49:47 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <algorithm>
+using namespace std;
+
+long long solution(long long n) {
+    string temp = to_string(n);
+    sort(temp.begin(), temp.end(), greater<>());
+    return stoll(temp);
+}
