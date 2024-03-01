64a042e8f4cda1562e6d4d00f06fe331f400c238
batsalee <109213754+batsalee@users.noreply.github.com>
2024-03-01 10:54:00 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.20 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int solution(string myString, string pat) {
+    transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
+    transform(pat.begin(), pat.end(), pat.begin(), ::tolower);
+    
+    if(myString.find(pat) == string::npos) return 0;
+    else return 1;
+}
