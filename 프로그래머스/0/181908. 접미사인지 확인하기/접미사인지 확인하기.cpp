827f0cb4bf245c53d8409a04ecaed32656642508
batsalee <109213754+batsalee@users.noreply.github.com>
2024-03-17 10:46:24 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -0,0 +1,7 @@
+#include <string>
+using namespace std;
+
+int solution(string my_string, string is_suffix) {
+    if(my_string.size() < is_suffix.size()) return 0;
+    else return my_string.substr(my_string.size() - is_suffix.size()) == is_suffix;
+}
