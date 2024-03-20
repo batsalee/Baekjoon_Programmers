65c425ce9b55003bc69cf458bdb9b71e82e3e227
batsalee <109213754+batsalee@users.noreply.github.com>
2024-03-21 07:39:43 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,8 @@
+#include <cmath>
+using namespace std;
+
+int solution(int a, int b) {
+    if(a % 2 == 1 && b % 2 == 1) return pow(a, 2) + pow(b, 2);
+    else if(a % 2 == 0 && b % 2 == 0) return abs(a - b);
+    else return 2 * (a + b);
+}
