676dbdd94b53a53c60eab7685c1886a18c819c1a
batsalee <109213754+batsalee@users.noreply.github.com>
2024-04-12 18:39:14 +0900

    [Gold V] Title: 두 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 2147483647
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	vector<int> values(N);
+	for (int i = 0; i < N; i++) {
+		cin >> values[i];
+	}
+	sort(values.begin(), values.end());
+
+	int l = 0, r = N - 1;
+	int alkaline = values.front(), acid = values.back(), sum = INF;
+	while (l < r) {
+		if (abs(values[l] + values[r]) < sum) {
+			alkaline = values[l];
+			acid = values[r];
+			sum = abs(values[l] + values[r]);
+		}
+
+		if (values[l] + values[r] == 0) break;
+
+		if (values[l] + values[r] < 0) l++;
+		else if (values[l] + values[r] > 0) r--;
+	}
+
+	cout << alkaline << ' ' << acid;
+
+	return 0;
+}
