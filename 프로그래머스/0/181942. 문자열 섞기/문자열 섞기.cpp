dc9133d6bce5a1c8f539ca53459eaf01b8552606
batsalee <109213754+batsalee@users.noreply.github.com>
2024-04-24 03:28:25 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+
+string solution(string str1, string str2) {
+    string answer = "";
+    for(int i = 0; i < str1.size(); i++) {
+        answer = answer + str1[i] + str2[i];
+    }
+    return answer;
+}
