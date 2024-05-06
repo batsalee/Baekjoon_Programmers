6c680e25ca76a237732c77e34661758c2d7f5db7
batsalee <109213754+batsalee@users.noreply.github.com>
2024-05-06 14:03:47 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,9 @@
+#include <string>
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    if(ineq == ">" && eq == "=") return n >= m;
+    else if(ineq == "<" && eq == "=") return n <= m;
+    else if(ineq == ">" && eq == "!") return n > m;
+    else if(ineq == "<" && eq == "!") return n < m;
+}
