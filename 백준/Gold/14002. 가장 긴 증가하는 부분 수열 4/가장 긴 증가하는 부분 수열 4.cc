9524e56261f5f415c4acd59e809c2dc0cb3c9cb8
batsalee <109213754+batsalee@users.noreply.github.com>
2024-07-03 08:04:54 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 2147483647
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> input(N), index(N);;
+    for (int i = 0; i < N; i++) { cin >> input[i]; }
+
+    vector<int> lis_vec;
+    lis_vec.push_back(-INF);
+    for (int i = 0; i < N; i++) {
+        if (lis_vec.back() < input[i]) {
+            lis_vec.push_back(input[i]);
+            index[i] = lis_vec.size() - 1;
+        }
+        else {
+            auto itr = lower_bound(lis_vec.begin(), lis_vec.end(), input[i]);
+            *itr = input[i];
+            index[i] = itr - lis_vec.begin();
+        }
+    }
+
+    int lis = lis_vec.size() - 1;
+    cout << lis << '\n';
+
+    vector<int> answer;
+    for (int i = N - 1; i >= 0; i--) {
+        if (index[i] == lis) {
+            answer.push_back(input[i]);
+            lis--;
+        }
+    }
+
+    for (auto ritr = answer.rbegin(); ritr != answer.rend(); ritr++) {
+        cout << *ritr << ' ';
+    }
+
+    return 0;
+}
