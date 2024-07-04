2be30e367ceb3991c559d3df50235a44ec8a8f22
batsalee <109213754+batsalee@users.noreply.github.com>
2024-07-05 00:36:34 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 352 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> parent;
+
+int Find(int x)
+{
+	if (parent[x] == x) return x;
+	return parent[x] = Find(parent[x]);
+}
+
+void Union(int x, int y)
+{
+	x = Find(x);
+	y = Find(y);
+	if (x < y) parent[y] = x;
+	else if (x > y) parent[x] = y;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	if (N == 2) {
+		cout << 0;
+		return 0;
+	}
+
+	parent.resize(N + 1);
+	for (int i = 1; i < N + 1; i++) {
+		parent[i] = i;
+	}
+
+	vector<tuple<int, int, int>> costs(M); // cost, from, to
+	int A, B, C;
+	for (int i = 0; i < M; i++) {
+		cin >> A >> B >> C;
+		costs[i] = {C, A, B}; // sort 쉽게하려고 위치 바꿔주고
+	}
+	sort(costs.begin(), costs.end());
+
+	int answer = 0, count = 0;
+	for (auto t : costs) {
+		int cost = get<0>(t);
+		int from = get<1>(t);
+		int to = get<2>(t);
+
+		if (Find(from) != Find(to)) {
+			answer += cost;
+			Union(from, to);
+			count++;		
+		}
+
+		if (count == N - 2) break; 
+		// N - 1이면 모든 집 연결인데 N - 2라서 마지막 젤 비싼 집 연결 안해줌
+	}
+
+	cout << answer;
+
+	return 0;
+}
