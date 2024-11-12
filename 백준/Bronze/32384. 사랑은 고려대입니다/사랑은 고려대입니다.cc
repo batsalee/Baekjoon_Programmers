a738818007fccf56c0743835a772c4ce9ee9d373
batsalee <109213754+batsalee@users.noreply.github.com>
2024-11-12 22:16:45 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	cout << "LoveisKoreaUniversity";
+
+	for (int i = 0; i < N - 1; i++) {
+		cout << " LoveisKoreaUniversity";
+	}
+
+	return 0;
+}
